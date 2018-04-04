431b951c4f81fbd4f0d763698211be99c5d15be9
Adam Dilger <adamdilger1@gmail.com>
2018-04-04 22:50:39 +1000

    finished options inputs

--- DataHelper.js
+++ DataHelper.js
@@ -1,4 +1,25 @@
 var InitialData = {
     envList: 'downerdev1',
-    queryList: {list: []}
+    queryList: JSON.stringify({list: [
+        {
+            label: "Business Rule",
+            table: "sys_script",
+            field: "Name",
+            operator: "like"
+        }
+    ]})
+}
+
+function getDataObject(envArray, queryArray) {
+    return {
+        queryList: JSON.stringify({list: queryArray}),
+        envList: envArray.join()
+    };
+}
+
+function retrieveValuesFromObject(items) {
+    return {
+        queries: JSON.parse(items.queryList).list,
+        envList: items.envList.split(',')
+    };
 }
--- options.js
+++ options.js
@@ -1,5 +1,11 @@
+var statusHeader;
+var queryContainer;
+var addQueryBtn;
+
 var queryCount = 0;
 var queries = [];
+var envList = [];
+var hasNewQuery = false;
 
 // Saves options to chrome.storage.sync.
 function save_options() {
@@ -27,59 +33,81 @@ function save_options() {
         queries.push(tmp);
     }
 
+    queries = queries.filter(item => {
+        return (item.field != '' && item.label != '' && item.operator != '' && item.table != '');
+    });
+
     envList = envList.filter(x => { return x != '' });
 
-    chrome.storage.sync.set({
-        queryList: {list: JSON.stringify(queries)},
-        envList: envList.join()
-    }, function() {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 1000);
-    });
+    chrome.storage.sync.set(getDataObject(envList, queries), 
+        function() {
+            statusHeader.textContent = 'Options saved.';
+            setTimeout(function() {
+                statusHeader.textContent = '';
+            }, 1000);
+
+            queryContainer.innerHTML = '';
+            addQueryBtn.disabled = false;
+            hasNewQuery = false;
+            refreshTables();
+        });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
 
-    var envList = [];
-
     chrome.storage.sync.get(InitialData, function(items) {
 
-        queries = JSON.parse(items.queryList.list);
+        var tmp = retrieveValuesFromObject(items);
+        queries = tmp.queries;
         queryCount = queries.length;
-        envList = items.envList.split(',');
+        envList = tmp.envList;
 
         for (var i = 0; i < envList.length; i++) {
             document.getElementById('env-url-input' + (i + 1)).value = envList[i];
         }
-    });
-    document.getElementById('save-options').addEventListener('click', save_options);
 
-    refreshTables();
+        refreshTables();
+    });
+    
+    addQueryBtn = document.getElementById('add-query');
+    statusHeader = document.getElementById('status');
+    queryContainer = document.getElementById('query-container');
+
+    document.getElementById('save-options').addEventListener('click', save_options);
+    addQueryBtn.addEventListener('click', function() {
+        queryCount++;
+        addQuery();
+    });
+    
 }
 
 function refreshTables() {
     var tableContainer = document.getElementById('table-container');
     tableContainer.innerHTML = '';
     for (var i = 0; i < queryCount; i++) {
+
         tableContainer.innerHTML += 
+            '<input type="text" name="label" placeholder="Label" id="label'+i+'" value="'+ queries[i].label +'">'+
+            '<input type="text" name="table" placeholder="Table Name" id="table'+i+'" value="'+ queries[i].table +'">'+
+            '<input type="text" name="field" placeholder="Field Name" id="field'+i+'" value="'+ queries[i].field +'">' +
+            getOperatorSelectBox(i) + ((i!=queryCount-1) ? '<br>' : '');
+    }
+}
+
+function addQuery() {
+    if (!hasNewQuery) {
+        var i = queryCount - 1;
+        queryContainer.innerHTML += 
             '<input type="text" name="label" placeholder="Label" id="label'+i+'">'+
             '<input type="text" name="table" placeholder="Table Name" id="table'+i+'">'+
             '<input type="text" name="field" placeholder="Field Name" id="field'+i+'">' +
-            getOperatorSelectBox(i) + ((i!=queryCount-1) ? '<br>' : '');
+        getOperatorSelectBox(i) + ((i!=queryCount-1) ? '<br>' : '');
+
+        addQueryBtn.disabled = true;
+        hasNewQuery = true;
     }
-
-    tableContainer.innerHTML += '<input id="add-query" type="button" value="+">';
-
-    document.getElementById('add-query').addEventListener('click', function() {
-        queryCount++;
-        refreshTables();
-    });
 }
 
 function getOperatorSelectBox(id) {
